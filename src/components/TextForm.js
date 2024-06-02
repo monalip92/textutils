172de924efc8d1dadb3c586f0107a4946444162d
monalip92 <monalip92@gmail.com>
2024-06-02 19:19:25 +0530

    video 8 completed

---
+++ src/components/TextForm.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    /// console.log("Uppercase clicked" + text);
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+
+  const handlelowClick = () => {
+    /// console.log("Lowercase clicked" + text);
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+
+  const handleOnChange = (event) => {
+    //console.log("On Change");
+    setText(event.target.value);
+  };
+  const [text, setText] = useState("");
+
+  return (
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="mybox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handlelowClick}>
+          Convert to Lowercase
+        </button>
+      </div>
+      <div className="container my-3">
+        <h1>Your Text Summary</h1>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>{0.08 * text.split(" ").length} minutes to read this words</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
+  );
+}
